743ee7cba69ca70a99ea9e44efba00c17d7d632c
Vasyl Shpak <vshpak@engenious.io>
2024-08-21 00:24:23 +0200

    add description

---
+++ tests/playwright-workshop-test.spec.ts
@@ -0,0 +1,14 @@
+//? Test Steps:
+// Open the Booking.com homepage.
+// Accept cookies
+// Locate the search input field labeled "Where are you going?"
+// Enter "Florida" into the search input field.
+// Select the check-in and check-out dates from the calendar.
+// Choose the number of guests and rooms.
+// Click the "Search" button.
+// Wait for the search results page to load.
+
+//? Expected Results:
+// The search results page should load successfully.
+// The page should display a list of hotels located in Florida.
+// The "Sort by" options and filters should be visible on the page.
