ea70278607e366d1edcc62c6589ee28381d7ebe8
Vasyl Shpak <vshpak@engenious.io>
2024-08-27 22:46:18 +0200

    update scenario

--- tests/playwright-workshop-test.spec.ts
+++ tests/playwright-workshop-test.spec.ts
@@ -2,7 +2,7 @@
 // Open the Booking.com homepage.
 // Accept cookies
 // Locate the search input field labeled "Where are you going?"
-// Enter "Florida" into the search input field.
+// Enter "Miami" into the search input field.
 // Select the check-in and check-out dates from the calendar.
 // Choose the number of guests and rooms.
 // Click the "Search" button.
@@ -10,5 +10,5 @@
 
 //? Expected Results:
 // The search results page should load successfully.
-// The page should display a list of hotels located in Florida.
+// The page should display a list of hotels located in Miami.
 // The "Sort by" options and filters should be visible on the page.
